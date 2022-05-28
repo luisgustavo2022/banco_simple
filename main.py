8193e5d6161045ab88f913a53704388b64b22349
Luis Gustavo F Freire <99625415+luisgustavo2022@users.noreply.github.com>
2022-05-28 01:33:45 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+class Conta:
+    def __init__(self, codigo, nome):
+        self._codigo = codigo
+        self._nome = nome
+        self._saldo = 0
+    def Depositar(self, dep):
+        self._saldo = self._saldo + dep
+    def Sacar(self, saq):
+        self._saldo = self._saldo - saq
+    def Saldo(self):
+        print(self._saldo)
+
+de
+
+
+
+
